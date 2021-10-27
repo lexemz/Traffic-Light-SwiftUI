c763bd295062702884bd9a2ec2516f966b64d408
Igor Buzykin <lexemz@gmail.com>
2021-10-27 12:12:28 +0300

    Initial Commit

---
+++ Traffic Light/Traffic_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_LightApp.swift
+//  Traffic Light
+//
+//  Created by Igor on 27.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
