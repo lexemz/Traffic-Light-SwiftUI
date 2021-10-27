6c4c1182dfd86f450fe429f919ff7f819184ba54
Igor Buzykin <lexemz@gmail.com>
2021-10-27 18:21:25 +0300

    Update CircleView

--- Traffic Light/CircleView.swift
+++ Traffic Light/CircleView.swift
@@ -8,19 +8,21 @@
 import SwiftUI
 
 struct CircleView: View {
-    let foregroundColor: Color
+    let color: Color
+    let opacity: Double
     
     var body: some View {
         Circle()
             .frame(width: 100.0, height: 100.0)
-            .foregroundColor(foregroundColor)
-            .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            .foregroundColor(color)
+            .opacity(opacity)
+            .overlay(Circle().stroke(Color.gray, lineWidth: 4))
             .shadow(radius: 5)
     }
 }
 
 struct CircleView_Previews: PreviewProvider {
     static var previews: some View {
-        CircleView(foregroundColor: .yellow)
+        CircleView(color: .blue, opacity: 1.0)
     }
 }
